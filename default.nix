5159116359389305fba2e0b43f05b0706881c1e0
Florian Duraffourg <f.duraffourg@gmail.com>
2018-07-04 17:33:38 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, aeson, base, brick, lens, lens-aeson, microlens
+, random, scientific, stdenv, text, vector, vty, wreq
+}:
+mkDerivation {
+  pname = "cetui";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson base brick lens lens-aeson microlens random scientific text
+    vector vty wreq
+  ];
+  license = stdenv.lib.licenses.gpl3;
+}
